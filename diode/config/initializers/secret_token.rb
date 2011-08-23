9077ed7358b6ea83451a4634154f08d2e96c8d64
jihao <jacky.jihao@gmail.com>
2011-08-23 17:22:37 +0800

    rails new diode

---
+++ diode/config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+Diode::Application.config.secret_token = '99a877158a16d6d94a79df79a7f526cfdf666db637acd3262b005da289d5b320ac7a094a684f20639276f3d2ac4999e64aeec8ccf5d33d7bddf4654f96e99ba1'
