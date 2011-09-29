224a9a41d966444abaeac7d20d5c8457b89636db
jihao <jacky.jihao@gmail.com>
2011-09-29 20:57:56 +0800

    deleted old repository, re-created using rails 3.1.0 now
    and I am using github client for Mac, this GUI looks great

--- diode/config/initializers/secret_token.rb
+++ diode/config/initializers/secret_token.rb
@@ -4,4 +4,4 @@
 # If you change this key, all old signed cookies will become invalid!
 # Make sure the secret is at least 30 characters and all random,
 # no regular words or you'll be exposed to dictionary attacks.
-Diode::Application.config.secret_token = '99a877158a16d6d94a79df79a7f526cfdf666db637acd3262b005da289d5b320ac7a094a684f20639276f3d2ac4999e64aeec8ccf5d33d7bddf4654f96e99ba1'
+Diode::Application.config.secret_token = 'f4fd6640801321216ae51d771f8240038a365f1d5852a8d304d58a3d9397746a6c7591b7cd279f7a5b0254a16192158d5b0dcef18e316f1a3e35e2a580cbd838'
