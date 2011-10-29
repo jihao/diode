a64dcf8208f28e895588ea6f45fece263e05591e
jihao <jacky.jihao@gmail.com>
2011-10-29 17:09:41 +0800

    add avatar helper method without any of the 3pp avatar gem

--- diode/app/helpers/application_helper.rb
+++ diode/app/helpers/application_helper.rb
@@ -1,2 +1,15 @@
 module ApplicationHelper
+  # helper method for generate user profile avatar image url
+  # availble options example: {:s=>200 , :r=>pg , :d=>404}
+  # reference: http://en.gravatar.com/site/implement/images/ http://en.gravatar.com/site/implement/images/ruby/
+  def avatar(user, options={})
+    if ((user.has_attribute? :image_url) && user.image_url.present?) then
+      user.image_url
+    else
+      gravatar_id = Digest::MD5.hexdigest(user.email.downcase)
+      options = {:s=>48 , :r=>"pg" , :d=>"mm"}.merge(options)
+      url_options = '?'+options.map {|k,v|  [k.to_s, v.to_s].join('=')}.join('&')
+      image_url = "http://gravatar.com/avatar/#{gravatar_id}.png#{url_options}"
+    end
+  end
 end
